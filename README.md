96572c36195ba41d5f934fa2b1416a6309e03960
Abu Desai <abhyuday.desai@gmail.com>
2022-08-14 19:01:52 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Random Forest Regressor using SKLearn for Regression - Base problem category as per Ready Tensor specifications.
+Model also has Shapley local explanations.
---
+++ app/algorithm/model/regressor.py
@@ -0,0 +1,78 @@
+#Import required libraries
+from random import Random
+import numpy as np, pandas as pd
+import joblib
+import sys
+import os, warnings
+warnings.filterwarnings('ignore') 
+
+from sklearn.ensemble import RandomForestRegressor
+
+
+model_fname = "model.save"
+MODEL_NAME = "regression_class_random_forest_shapley"
+
+class Regressor(): 
+    
+    def __init__(self, n_estimators = 100, max_features = 1, max_samples = 0.5, **kwargs) -> None:
+        self.n_estimators = int(n_estimators)
+        self.max_features = int(max_features)
+        self.max_samples= np.float(max_samples)
+        
+        self.model = self.build_model()
+        
+        
+        
+    def build_model(self): 
+        model = RandomForestRegressor(n_estimators= self.n_estimators, max_features= self.max_features, 
+                                      max_samples= self.max_samples, random_state=42, 
+                                    #   criterion='squared_error',
+                                      bootstrap= True, 
+                                      oob_score= True, 
+                                      n_jobs=-1, verbose=0)
+        return model
+    
+    
+    def fit(self, train_X, train_y):        
+        self.model.fit(
+                X = train_X,
+                y = train_y)
+    
+    
+    def predict(self, X, verbose=False): 
+        preds = self.model.predict(X)
+        return preds 
+    
+
+    def summary(self):
+        self.model.get_params()
+        
+    
+    def evaluate(self, x_test, y_test): 
+        """Evaluate the model and return the loss and metrics"""
+        if self.model is not None:
+            return self.model.score(x_test, y_test)        
+
+    
+    def save(self, model_path): 
+        joblib.dump(self.model, os.path.join(model_path, model_fname))
+
+    @classmethod
+    def load(cls, model_path): 
+        rf = joblib.load(os.path.join(model_path, model_fname))
+        return rf
+
+
+def save_model(model, model_path):    
+    model.save(model_path) 
+    
+
+def load_model(model_path): 
+    try: 
+        model = Regressor.load(model_path)        
+    except: 
+        raise Exception(f'''Error loading the trained {MODEL_NAME} model. 
+            Do you have the right trained model in path: {model_path}?''')
+    return model
+
+
